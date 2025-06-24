73212d8ef978248f306d5cf8c6a923141acd4763
lazy-media <130882130+lazy-media@users.noreply.github.com>
2025-06-24 05:40:37 +0000

    Update Files:
    Revert Files: openai.tsx
    Edit index.tsx

--- apps/client/src/pages/dashboard/settings/_sections/openai.tsx
+++ apps/client/src/pages/dashboard/settings/_sections/openai.tsx
@@ -106,9 +106,9 @@ export const OpenAISettings = () => {
 
         <p>
           <Trans>
-            You can also integrate with OpenWebUI simply by setting an API key like
-            `sk-1234567890abcdef` and the Base URL to your OpenWebUI URL over HTTPS, i.e.
-            `https://localhost:8080/api`. You can also pick and choose models and set the max tokens
+            You can also integrate with Ollama simply by setting the API key to
+            `sk-1234567890abcdef` and the Base URL to your Ollama URL, i.e.
+            `http://localhost:11434/v1`. You can also pick and choose models and set the max tokens
             as per your preference.
           </Trans>
         </p>
