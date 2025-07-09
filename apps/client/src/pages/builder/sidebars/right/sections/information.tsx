935d923ff0af71270f0739552e62b81cddbe9249
lazy-media <130882130+lazy-media@users.noreply.github.com>
2025-07-09 04:12:59 +0000

    Update Sidebar information. Didn't like it was worded before.

--- apps/client/src/pages/builder/sidebars/right/sections/information.tsx
+++ apps/client/src/pages/builder/sidebars/right/sections/information.tsx
@@ -19,11 +19,11 @@ const DonateCard = () => (
       <CardDescription className="space-y-2">
         <Trans>
           <p>
-            I single-handedly resurrected Reactive Resume from the depths of abandoned GitHub repos,
-            _you’re welcome_, fixed its existential crises, gave it a shiny public home, and now
-            play full-time bug whisperer—all while gently nudging the community to maybe, just
-            maybe, help out. Huge thanks to the open-source legends who kept me from losing my
-            sanity.
+            I single-handedly resurrected Reactive Resume from the depths of an abandoned GitHub
+            repo, fixed its existential crises, gave it a shiny public home, and now play full-time
+            bug whisperer—all while gently nudging the community to maybe, just maybe, help out.
+            Huge thanks to the open-source legends who kept this project alive. You all are the real
+            MVPs!
           </p>
           <p>
             Love the app? Keep it free forever by tossing a coin to your dev _or at least a virtual
